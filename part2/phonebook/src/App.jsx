b4efcdcf559612aba7d0445ebcc4d3e74a9b6427
mrrustemka <mubarakshin1996@gmail.com>
2025-08-03 11:02:38 +0500

    part-2-unit-c

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import PersonForm from './components/PersonForm';
 import Persons from './components/Persons';
+import axios from 'axios';
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('');
-  const [filteredPersons, setFilteredPersons] = useState(persons);
+  const [filterText, setFilterText] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(filterText.toLowerCase())
+  );
 
   function onSubmit(event) {
     event.preventDefault();
@@ -21,15 +27,18 @@ function App() {
       phone: newPhone,
       id: persons.length + 1
     };
+
     if (persons.some((person) => person.name === newName)) {
       alert(`${newName} is already added to phonebook`);
       setNewName('');
       return;
     }
+
     setPersons(persons.concat(personObject));
     setNewName('');
     setNewPhone('');
   }
+
   function handleNameChange(event) {
     setNewName(event.target.value);
   }
@@ -39,11 +48,7 @@ function App() {
   }
 
   function handleFilterChange(event) {
-    setFilteredPersons(
-      persons.filter((person) =>
-        person.name.toLowerCase().includes(event.target.value.toLowerCase())
-      )
-    );
+    setFilterText(event.target.value);
   }
 
   return (
