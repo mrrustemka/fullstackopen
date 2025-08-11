0c3a63fe26527ddd63554355f142541aed2b8fc2
mrrustemka <mubarakshin1996@gmail.com>
2025-08-11 22:50:16 +0500

    part-3-fix-deploy

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,6 +2,7 @@
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+app.use(express.static('dist'));
 
 app.use(cors());
 
@@ -70,7 +71,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
