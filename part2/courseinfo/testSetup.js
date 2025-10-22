0aa9573e352f39d00e90bd819e5b31fd150f5a43
mrrustemka <mubarakshin1996@gmail.com>
2025-10-22 21:09:01 +0500

    part-5-testing-react-apps-rendering-the-component-for-tests

---
+++ part2/courseinfo/testSetup.js
@@ -0,0 +1,27 @@
+﻿/* eslint-disable semi */
+import { afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
+
+afterEach(() => {
+  cleanup();
+});
+
+export default defineConfig({
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './testSetup.js'
+  }
+});
+
+const Note = ({ note, toggleImportance }) => {
+  const label = note.important ? 'make not important' : 'make important';
+
+  return (
+    <li className='note'>
+      {note.content}
+      <button onClick={toggleImportance}>{label}</button>
+    </li>
+  );
+};
