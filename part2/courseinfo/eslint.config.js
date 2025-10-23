419fb16b7fc38255da80cd7daaf67c49ef9c65cb
mrrustemka <mubarakshin1996@gmail.com>
2025-10-23 22:16:08 +0500

    part-5-testing-react-apps-rendering-the-component-for-tests

--- part2/courseinfo/eslint.config.js
+++ part2/courseinfo/eslint.config.js
@@ -30,14 +30,22 @@ export default [
         { allowConstantExport: true }
       ],
       indent: ['error', 2],
-      'linebreak-style': ['error', 'unix'],
+      // 'linebreak-style': ['error', 'unix'],
       quotes: ['error', 'single'],
-      semi: ['error', 'never'],
+      // semi: ['error', 'never'],
       eqeqeq: 'error',
       'no-trailing-spaces': 'error',
       'object-curly-spacing': ['error', 'always'],
       'arrow-spacing': ['error', { before: true, after: true }],
       'no-console': 'off'
+    }
+  },
+  {
+    files: ['**/*.test.{js,jsx}'],
+    languageOptions: {
+      globals: {
+        ...globals.vitest
+      }
     }
   }
 ];
