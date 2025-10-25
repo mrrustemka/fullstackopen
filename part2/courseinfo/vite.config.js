a5a2b973968a527838b124528ee742f68041e1f4
mrrustemka <mubarakshin1996@gmail.com>
2025-10-25 21:29:37 +0500

    part-5-testing-react-apps-test-coverage

--- part2/courseinfo/vite.config.js
+++ part2/courseinfo/vite.config.js
@@ -7,6 +7,12 @@ export default defineConfig({
   test: {
     environment: 'jsdom',
     globals: true,
-    setupFiles: './testSetup.js'
+    setupFiles: './testSetup.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      include: ['src/**/*.jsx', 'src/**/*.js'],
+      all: true
+    }
   }
 });
