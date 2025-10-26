f7158b8e48f86a55da0f4ad35af7ae76a4410bf9
mrrustemka <mubarakshin1996@gmail.com>
2025-10-26 14:49:21 +0500

    part-5-testing-react-apps-5-15

--- part4/bloglist-frontend/src/components/Blog.test.jsx
+++ part4/bloglist-frontend/src/components/Blog.test.jsx
@@ -42,9 +42,38 @@ test('Does blog URL and likes shown', async () => {
   const button = screen.getByText('View');
   await user.click(button);
 
-  //   const url = screen.getByText('url');
-  //   const likes = screen.getByText('Likes');
-  //   expect(url).toBeInTheDocument();
   expect(screen.getByText(/Likes/i)).toBeInTheDocument();
   expect(screen.getByText('rustem')).toBeInTheDocument();
+});
+
+test('Does like button calls twice', async () => {
+  const blog = {
+    title: 'Title',
+    author: 'Author',
+    url: 'my_url',
+    likes: 5,
+    user: { username: 'rustem', id: '1' }
+  };
+
+  const mockSetLikes = vi.fn();
+  const mockRemove = vi.fn();
+  const user = userEvent.setup();
+
+  render(
+    <Blog
+      blog={blog}
+      setBlogLikes={mockSetLikes}
+      remove={mockRemove}
+      user={{ id: '1' }}
+    />
+  );
+
+  const buttonView = screen.getByText('View');
+  await user.click(buttonView);
+
+  const buttonLike = screen.getByText('Like');
+  await user.click(buttonLike);
+  await user.click(buttonLike);
+
+  expect(mockSetLikes).toHaveBeenCalledTimes(2);
 });
