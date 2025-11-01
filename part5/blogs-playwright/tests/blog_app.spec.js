278647bad792d78aed7fc3c7bb9e0400af244960
mrrustemka <mubarakshin1996@gmail.com>
2025-11-01 20:44:11 +0500

    part-5-end-to-end-testing-playwright-5-17

---
+++ part5/blogs-playwright/tests/blog_app.spec.js
@@ -0,0 +1,14 @@
+﻿const { test, expect, beforeEach, describe } = require('@playwright/test');
+const { loginWith } = require('./heper');
+
+describe('Blog app', () => {
+  beforeEach(async ({ page }) => {
+    await page.goto('/');
+  });
+
+  test('Login form is shown', async ({ page }) => {
+    loginWith(page, 'test', 'test');
+
+    await expect(page.getByText('test logged in')).toBeVisible();
+  });
+});
