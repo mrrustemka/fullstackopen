6e41b8648e8a8846ef52a78b9d27db86f409201e
mrrustemka <mubarakshin1996@gmail.com>
2025-11-02 11:49:39 +0500

    part-5-end-to-end-testing-playwright-5-18

--- part5/blogs-playwright/tests/blog_app.spec.js
+++ part5/blogs-playwright/tests/blog_app.spec.js
@@ -2,13 +2,35 @@
 const { loginWith } = require('./heper');
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    // I don't want to reset users in DB every time
+    // I created test user once and would like to use it
+
+    // await request.post('/api/testin/reset');
+    // await request.post('/api/users', {
+    //   data: {
+    //     name: 'PW test',
+    //     username: 'PW test',
+    //     password: 'PW test'
+    //   }
+    // });
     await page.goto('/');
   });
 
   test('Login form is shown', async ({ page }) => {
-    loginWith(page, 'test', 'test');
+    await expect(page.getByRole('button', { name: 'Login' })).toBeVisible();
+  });
 
-    await expect(page.getByText('test logged in')).toBeVisible();
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      loginWith(page, 'test', 'test');
+
+      await expect(page.getByText('test logged in')).toBeVisible();
+    });
+
+    test('fails with wrong credentials', async ({ page }) => {
+      loginWith(page, 'test', 'wrong');
+      await expect(page.getByText('Wrong username or password')).toBeVisible();
+    });
   });
 });
