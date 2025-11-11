6afacf433b647f1c131605136e51e6999f428a7f
mrrustemka <mubarakshin1996@gmail.com>
2025-11-11 11:16:17 +0500

    flux-architecture-and-redux-tests-for-note-reducer

--- part6/notes/src/reducers/noteReducer.js
+++ part6/notes/src/reducers/noteReducer.js
@@ -1,8 +1,16 @@
 ﻿const noteReducer = (state = [], action) => {
   switch (action.type) {
     case 'NEW_NOTE':
-      state.concat(action.payload);
-      return state;
+      return state.concat(action.payload);
+    case 'TOGGLE_IMPORTANCE': {
+      const id = action.payload.id;
+      const noteToChange = state.find((n) => n.id === id);
+      const changedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
+      };
+      return state.map((note) => (note.id !== id ? note : changedNote));
+    }
     default:
       return state;
   }
