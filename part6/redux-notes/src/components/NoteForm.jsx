a2aa3eca29fbd5c68f145e1f1b4acdb0940eca81
mrrustemka <mubarakshin1996@gmail.com>
2025-11-13 11:29:01 +0500

    flux-architecture-and-redux-more-components

---
+++ part6/redux-notes/src/components/NoteForm.jsx
@@ -0,0 +1,22 @@
+﻿import { useDispatch } from 'react-redux';
+import { createNote } from '../reducers/noteReducer';
+
+function NoteForm() {
+  const dispatch = useDispatch();
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const content = event.target.note.value;
+    event.target.note.value = '';
+    dispatch(createNote(content));
+  };
+
+  return (
+    <form onSubmit={addNote}>
+      <input name='note' />
+      <button type='submit'>Add</button>
+    </form>
+  );
+}
+
+export default NoteForm;
