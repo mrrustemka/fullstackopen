3fd5b239b6f86139bae8f70b86edf3adef7b5814
mrrustemka <mubarakshin1996@gmail.com>
2025-11-17 09:41:59 +0500

    part-6-store-with-complex-state

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -2,9 +2,33 @@ import NoteForm from './components/NoteForm';
 import Notes from './components/Notes';
 
 const App = () => {
+  const filterSelected = (value) => {
+    console.log(value);
+  };
+
   return (
     <>
       <NoteForm />
+      <div>
+        <input
+          type='radio'
+          name='filter'
+          onChange={() => filterSelected('ALL')}
+        />
+        All
+        <input
+          type='radio'
+          name='filter'
+          onChange={() => filterSelected('IMPORTANT')}
+        />
+        Important
+        <input
+          type='radio'
+          name='filter'
+          onChange={() => filterSelected('NONIMPORTANT')}
+        />
+        Non-important
+      </div>
       <Notes />
     </>
   );
