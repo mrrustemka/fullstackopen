178bab1f9275f335f03392750005837ae4f97d6e
mrrustemka <mubarakshin1996@gmail.com>
2025-11-17 09:52:18 +0500

    part-6-mane-reducers-combined-reducers

--- part6/redux-notes/src/main.jsx
+++ part6/redux-notes/src/main.jsx
@@ -1,14 +1,31 @@
-import ReactDOM from 'react-dom/client'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
+import ReactDOM from 'react-dom/client';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
-import App from './App'
-import noteReducer from './reducers/noteReducer'
+import App from './App';
+import noteReducer from './reducers/noteReducer';
+import filterReducer from './reducers/filterReducer';
 
-const store = createStore(noteReducer)
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer
+});
+
+const store = createStore(noteReducer);
+
+console.log(store.getState());
+
+import { createNote } from './reducers/noteReducer';
+import { filterChange } from './reducers/filterReducer';
+
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(filterChange('IMPORTANT'));
+store.dispatch(
+  createNote('combineReducers forms one reducer from many simple reducers')
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
-)
+);
