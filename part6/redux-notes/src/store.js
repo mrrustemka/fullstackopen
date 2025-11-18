fa1c95177c12456d46f074450938bdda630f0da4
mrrustemka <mubarakshin1996@gmail.com>
2025-11-18 09:42:55 +0500

    part-6-many-reducers-redux-toolkit-and-refactoring-store-config

---
+++ part6/redux-notes/src/store.js
@@ -0,0 +1,13 @@
+﻿import { configureStore } from '@reduxjs/toolkit';
+
+import noteReducer from './reducers/noteReducer';
+import filterReducer from './reducers/filterReducer';
+
+const store = configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
+});
+
+export default store;
