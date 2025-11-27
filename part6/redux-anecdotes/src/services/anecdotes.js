2255d47259438ad95810b4dcdede42f317cc8e3d
mrrustemka <mubarakshin1996@gmail.com>
2025-11-27 22:22:06 +0500

    part-6-communicating-with-server-in-a-redux-application-6-14

---
+++ part6/redux-anecdotes/src/services/anecdotes.js
@@ -0,0 +1,13 @@
+﻿const baseUrl = 'http://localhost:3001/anecdotes';
+
+const getAll = async () => {
+  const response = await fetch(baseUrl);
+
+  if (!response.ok) {
+    throw new Error('Failed to load anecdotes');
+  }
+
+  return await response.json();
+};
+
+export default { getAll };
