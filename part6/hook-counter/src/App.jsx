11e2842752fe1745d9dbe240d3765a48582896ed
mrrustemka <mubarakshin1996@gmail.com>
2025-12-01 00:15:35 +0500

    part-6-d-defining-the-counter-context-in-separate-file

--- part6/hook-counter/src/App.jsx
+++ part6/hook-counter/src/App.jsx
@@ -1,33 +1,16 @@
-import { useReducer } from 'react';
 import Button from './components/Button';
 import Display from './components/Display';
-import CounterContext from './CounterContext';
-
-const counterReducer = (state, action) => {
-  switch (action.type) {
-    case 'INC':
-      return state + 1;
-    case 'DEC':
-      return state - 1;
-    case 'ZERO':
-      return 0;
-    default:
-      return state;
-  }
-};
 
 const App = () => {
-  const [counter, counterDispatch] = useReducer(counterReducer, 0);
-
   return (
-    <CounterContext.Provider value={{ counter, counterDispatch }}>
+    <div>
       <Display />
       <div>
-        <Button label='+' type='INC' />
-        <Button label='-' type='DEC' />
-        <Button label='0' type='ZERO' />
+        <Button type='INC' label='+' />
+        <Button type='DEC' label='-' />
+        <Button type='ZERO' label='0' />
       </div>
-    </CounterContext.Provider>
+    </div>
   );
 };
 
--- part6/hook-counter/src/main.jsx
+++ part6/hook-counter/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-import App from './App'
+import App from './App';
+
+import { CounterContextProvider } from './CounterContext';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <CounterContextProvider>
+      <App />
+    </CounterContextProvider>
+  </StrictMode>
+);
