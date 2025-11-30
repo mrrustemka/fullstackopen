b7878fb01b9929b854bdd3fd40b0415013a1e598
mrrustemka <mubarakshin1996@gmail.com>
2025-11-30 21:42:30 +0500

    part-6-d-6-21

--- part6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,14 +1,41 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  const createAnecdote = async (content) => {
+    const options = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ content }),
+    }
+
+    const response = await fetch('http://localhost:3001/anecdotes', options)
+
+    if (!response.ok) {
+      throw new Error('Failed to create anecdote')
+    }
+    return await response.json()
+  }
+
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+    },
+  })
+
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
+    newAnecdoteMutation.mutate(content)
   }
 
   return (
     <div>
-      <h3>create new</h3>
+      <h3>Create new</h3>
       <form onSubmit={onCreate}>
         <input name="anecdote" />
         <button type="submit">create</button>
