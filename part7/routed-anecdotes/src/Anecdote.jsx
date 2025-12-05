25cbb5785fdf58e3a47bfd31919b58a037ecdcb2
mrrustemka <mubarakshin1996@gmail.com>
2025-12-05 14:24:55 +0500

    part-7-a-7-2

---
+++ part7/routed-anecdotes/src/Anecdote.jsx
@@ -0,0 +1,9 @@
+﻿function Anecdote({ anecdote }) {
+  return (
+    <li>
+      <h2>{anecdote.content}</h2>
+    </li>
+  );
+}
+
+export default Anecdote;
--- part7/routed-anecdotes/src/Menu.jsx
+++ part7/routed-anecdotes/src/Menu.jsx
@@ -1,12 +1,18 @@
-﻿import { Link, Routes, Route } from 'react-router-dom';
+﻿import { Link, Routes, Route, useMatch } from 'react-router-dom';
 import About from './About';
 import AnecdoteList from './AnecdoteList';
+import Anecdote from './Anecdote';
 import CreateNew from './CreateNew';
 
 const Menu = ({ addNew, anecdotes }) => {
   const padding = {
     paddingRight: 5
   };
+
+  const match = useMatch('/anecdotes/:id');
+  const anecdote = match
+    ? anecdotes.find((a) => a.id === Number(match.params.id))
+    : null;
 
   return (
     <div>
@@ -21,6 +27,10 @@ const Menu = ({ addNew, anecdotes }) => {
       </Link>
       <Routes>
         <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route
+          path='/anecdotes/:id'
+          element={<Anecdote anecdote={anecdote} />}
+        />
         <Route path='/create_new' element={<CreateNew addNew={addNew} />} />
         <Route path='/about' element={<About />} />
       </Routes>
