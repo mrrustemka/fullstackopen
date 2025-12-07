658b41148a946559b48788f334ad966480b3cc53
mrrustemka <mubarakshin1996@gmail.com>
2025-12-07 17:19:16 +0500

    part-7-b-7-4

--- part7/routed-anecdotes/src/CreateNew.jsx
+++ part7/routed-anecdotes/src/CreateNew.jsx
@@ -1,18 +1,19 @@
 ﻿import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useField } from './hooks';
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('');
-  const [author, setAuthor] = useState('');
-  const [info, setInfo] = useState('');
   const navigate = useNavigate();
+  const content = useField();
+  const author = useField();
+  const info = useField();
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     });
     navigate('/');
@@ -31,24 +32,27 @@ const CreateNew = (props) => {
           Content
           <input
             name='content'
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            value={content.value}
+            type={content.type}
+            onChange={content.onChange}
           />
         </div>
         <div>
           Author
           <input
             name='author'
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
+            value={author.value}
+            type={author.type}
+            onChange={author.onChange}
           />
         </div>
         <div>
           URL for more info
           <input
             name='info'
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
+            value={info.value}
+            type={info.type}
+            onChange={info.onChange}
           />
         </div>
         <button type='submit'>Create</button>
---
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -0,0 +1,11 @@
+﻿import { useState } from 'react';
+
+export const useField = (type) => {
+  const [value, setValue] = useState('');
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return { type, value, onChange };
+};
