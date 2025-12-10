9d27f49d5d2201188812a6b8d480c9703b5951f9
mrrustemka <mubarakshin1996@gmail.com>
2025-12-10 14:25:58 +0500

    part-7-d-loaders

--- part7/webpack/src/index.js
+++ part7/webpack/src/index.js
@@ -1,5 +1,1 @@
-﻿import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+console.log('test');
--- part7/webpack/webpack.cofig.js
+++ part7/webpack/webpack.cofig.js
@@ -1,32 +1,26 @@
-﻿const path = require('path');
+const path = require('path');
 
-const config = () => {
-  return {
-    entry: './src/index.js',
-    output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: 'main.js'
-    },
-
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react']
-            }
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
-      ]
-    },
-
-    resolve: {
-      extensions: ['.js', '.jsx']
-    }
-  };
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 };
-
-module.exports = config;
