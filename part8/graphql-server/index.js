159363ca65f90490e957cff18bdfd07b45c75a05
mrrustemka <mubarakshin1996@gmail.com>
2025-12-28 18:33:20 +0500

    part-8-a-error-handling

--- part8/graphql-server/index.js
+++ part8/graphql-server/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const { GraphQLError } = require('graphql');
 
 let persons = [
   {
@@ -69,6 +70,15 @@ const resolvers = {
   },
   Mutation: {
     addPerson: (root, args) => {
+      if (persons.find((p) => p / name === args.name)) {
+        throw new GraphQLError('Name must be unique', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name
+          }
+        });
+      }
+
       const person = { ...args, id: uuid() };
       persons = persons.concat(person);
       return person;
