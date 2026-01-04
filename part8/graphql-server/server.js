04550599cf86ca01dd6430dee535522b03e27e1b
mrrustemka <mubarakshin1996@gmail.com>
2026-01-04 15:17:43 +0500

    part-8-c-refactoring-backend

---
+++ part8/graphql-server/server.js
@@ -0,0 +1,20 @@
+﻿const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+const startServer = (port) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  startStandaloneServer(server, {
+    listen: { port: 4000 }
+  }).then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+  });
+};
+
+module.exports = startServer;
