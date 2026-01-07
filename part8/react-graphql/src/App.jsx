26ef6903d2941942a2da36de85b2b9f847b28d36
mrrustemka <mubarakshin1996@gmail.com>
2026-01-07 16:45:28 +0500

    part-8-c-user-login

--- part8/react-graphql/src/App.jsx
+++ part8/react-graphql/src/App.jsx
@@ -1,15 +1,20 @@
-import { useQuery } from '@apollo/client/react';
+import { useApolloClient, useQuery } from '@apollo/client/react';
 import { useState } from 'react';
 import Persons from './components/Persons';
 import PersonForm from './components/PersonForm';
 import PhoneForm from './components/PhoneForm';
 import { ALL_PERSONS } from './queries';
 import Notify from './components/Notify';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
 function App() {
   const result = useQuery(ALL_PERSONS);
   const [message, setMessage] = useState(null);
+  const [token, setToken] = useState(
+    localStorage.getItem('phonebook-user-token')
+  );
+  const client = useApolloClient();
 
   const notify = (m) => {
     setMessage(m);
@@ -18,13 +23,29 @@ function App() {
     }, 10000);
   };
 
+  const onLogout = () => {
+    setToken(null);
+    localStorage.removeItem('phonebook-user-token');
+    client.resetStore();
+  };
+
   if (result.loading) {
     return <>Loading...</>;
+  }
+
+  if (!token) {
+    return (
+      <>
+        <Notify message={message} />
+        <LoginForm setError={notify} setToken={setToken} />
+      </>
+    );
   }
 
   return (
     <>
       <Notify message={message} />
+      <button onClick={onLogout}>Logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
---
+++ part8/react-graphql/src/components/LoginForm.jsx
@@ -0,0 +1,50 @@
+﻿import { useMutation } from '@apollo/client/react';
+import { useState } from 'react';
+import { LOGIN } from '../queries';
+
+function LoginForm({ setError, setToken }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [login] = useMutation(LOGIN, {
+    onCompleted: (data) => {
+      const token = data.logn.value;
+      setToken(token);
+      localStorage.setItem('phonebook-user-token'.token);
+    },
+    onError: (error) => {
+      setError(error.message);
+    }
+  });
+
+  const submit = (event) => {
+    event.preventDefault();
+    login({ variables: { username, password } });
+  };
+
+  return (
+    <>
+      <form onSubmit={submit}>
+        <h2>Login</h2>
+        <div>
+          Username
+          <input
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          Password
+          <input
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+            type='password'
+          />
+        </div>
+        <button type='submit'>Login</button>
+      </form>
+    </>
+  );
+}
+
+export default LoginForm;
