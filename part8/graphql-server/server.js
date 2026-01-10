f5d3124ee84bb0dc054a3b5d77cd928460551a46
mrrustemka <mubarakshin1996@gmail.com>
2026-01-10 11:38:53 +0500

    part-8-d-express-midleware

--- part8/graphql-server/server.js
+++ part8/graphql-server/server.js
@@ -1,5 +1,13 @@
 ﻿const { ApolloServer } = require('@apollo/server');
 const { startStandaloneServer } = require('@apollo/server/standalone');
+const {
+  ApolloServerPluginDrainHttpServer
+} = require('@apollo/server/plugin/drainHttpServer');
+const { expressMiddleware } = reqire('@as-integrations/express5');
+const cors = require('cors');
+const express = reqire('express');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const http = require('http');
 const jwt = require('jsonwebtoken');
 
 const typeDefs = require('./schema');
@@ -15,22 +23,33 @@ const getUserFromAuthHeader = async (auth) => {
   return User.findById(decodedToken.id).populate('friends');
 };
 
-const startServer = (port) => {
+const startServer = async (port) => {
+  const app = express();
+  const httpServer = http.createServer(app);
+
   const server = new ApolloServer({
-    typeDefs,
-    resolvers
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
   });
 
-  startStandaloneServer(server, {
-    listen: { port: 4000 },
-    context: async ({ req }) => {
-      const auth = req.headers.authorication;
-      const currentUser = await getUserFromAuthHeader(auth);
-      return { currentUser };
-    }
-  }).then(({ url }) => {
-    console.log(`Server ready at ${url}`);
-  });
+  await server.start();
+
+  app.use(
+    '/',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const auth = req.headers.authorization;
+        const currentUser = await getUserFromAuthHeader(auth);
+        return { currentUser };
+      }
+    })
+  );
+
+  httpServer.listen(port, () =>
+    console.log(`Server is now running on http://localhost:${port}`)
+  );
 };
 
 module.exports = startServer;
