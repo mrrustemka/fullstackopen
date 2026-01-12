66e41b0ee7155b9ad1ce9420b68a61b9c8fd5316
mrrustemka <mubarakshin1996@gmail.com>
2026-01-12 22:16:08 +0500

    part-9-b-creating-your-first-own-types

---
+++ part9/typescript/multiplier.ts
@@ -0,0 +1,32 @@
+﻿type Operation = 'multiply' | 'add' | 'divide';
+
+const calculator = (a: number, b: number, printText: Operation): number => {
+  switch (printText) {
+    case 'multiply':
+      return a * b;
+    case 'divide':
+      if (b === 0) {
+        throw new Error(`Can't divide by 0!`);
+      }
+
+      return a / b;
+    case 'add':
+      return a + b;
+    default:
+      throw new Error('Operation is not multiply, add or divide');
+  }
+};
+
+try {
+  console.log(calculator(1, 2, 'multiply'));
+  console.log(calculator(10, 20, 'add'));
+  console.log(calculator(10, 2, 'divide'));
+} catch (error: unknown) {
+  let message = 'Something went wrong';
+
+  if (error instanceof Error) {
+    message += error.message;
+  }
+
+  console.log(message);
+}
