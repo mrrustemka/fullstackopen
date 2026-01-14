427605cb1ee3c43d3669363a70df355dd669f729
mrrustemka <mubarakshin1996@gmail.com>
2026-01-14 10:53:43 +0500

    part-9-b-9-2

---
+++ part9/exercises/exerciseCalculator.ts
@@ -0,0 +1,42 @@
+﻿interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+function getCalculations(hours: number[], target: number): Result {
+  const average =
+    hours.reduce((acc, cur) => {
+      return acc + cur;
+    }, 0) / hours.length;
+
+  return {
+    periodLength: hours.length,
+    trainingDays: hours.filter((h) => h > 0).length,
+    success: average >= target,
+    rating: average < 1 ? 1 : average < 2 ? 2 : 3,
+    ratingDescription: getRatingDescription(average),
+    target: target,
+    average: average
+  };
+}
+
+function getRatingDescription(rating: number): string {
+  if (rating < 1) {
+    return 'Bad';
+  } else if (rating < 2) {
+    return 'Not too bad but could be better';
+  } else {
+    return 'Nice job';
+  }
+}
+
+try {
+  console.log(getCalculations([3, 0, 2, 4.5, 0, 3, 1], 2));
+} catch (error) {
+  throw new Error(error.message);
+}
