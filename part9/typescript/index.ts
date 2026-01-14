9a6f490741939c391df546b30cbd902e7b8c2ea1
mrrustemka <mubarakshin1996@gmail.com>
2026-01-14 11:26:49 +0500

    part-9-b-adding-express-to-the-mix

---
+++ part9/typescript/index.ts
@@ -0,0 +1,13 @@
+﻿const express = require('express');
+
+const app = express();
+
+app.get('/ping', (_req: any, res: any) => {
+  res.send('pong');
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
