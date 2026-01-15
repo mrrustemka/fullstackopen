e1255ba346fe08eeb3ed8ec1022880da423edd95
mrrustemka <mubarakshin1996@gmail.com>
2026-01-15 12:29:37 +0500

    part-9-b-9-4

---
+++ part9/exercises/index.ts
@@ -0,0 +1,12 @@
+﻿const express = require('express');
+const app = express();
+
+app.get('/ping', (_req: any, res: any) => {
+  res.send('pong');
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
