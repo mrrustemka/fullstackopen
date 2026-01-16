c58ea9c4caaed2f95d5baee61d493b6fe327b0b1
mrrustemka <mubarakshin1996@gmail.com>
2026-01-16 19:47:10 +0500

    part-9-b-type-assertion

--- part9/typescript/index.ts
+++ part9/typescript/index.ts
@@ -1,6 +1,6 @@
 ﻿const express = require('express');
 const app = express();
-import { calculator } from './calculator';
+import { calculator, Operation } from './calculator';
 
 app.use(express.json());
 
@@ -14,13 +14,13 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.post('/calculate', (req, res) => {
+app.post('/calculate', (req: any, res: any) => {
   const { value1, value2, op } = req.body;
 
   if (!value1 || isNaN(Number(value1))) {
     return res.status(400).send({ error: '...' });
   }
 
-  const result = calculator(Number(value1), Number(value2), op);
+  const result = calculator(Number(value1), Number(value2), op as Operation);
   res.send({ result });
 });
