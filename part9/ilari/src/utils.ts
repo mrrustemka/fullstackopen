3926ee7e1b1e90d0d0d15e9bdb5c85f8561c385d
mrrustemka <mubarakshin1996@gmail.com>
2026-01-20 09:37:20 +0500

    part-9-c-type-guards

--- part9/ilari/src/utils.ts
+++ part9/ilari/src/utils.ts
@@ -1,4 +1,4 @@
-﻿import { newDiaryEntry } from './types';
+﻿import { newDiaryEntry, Visibility, Weather } from './types';
 
 export const toNewDiaryEntry = (object: unknown): newDiaryEntry => {
   console.log(object);
@@ -10,4 +10,50 @@ export const toNewDiaryEntry = (object: unknown): newDiaryEntry => {
   };
 
   return newEntry;
+};
+
+const parseComment = (comment: unknown): string => {
+  if (!comment || !isString(comment)) {
+    throw new Error('Incorrect or missing comment');
+  }
+
+  return comment;
+};
+
+const parseDate = (date: unknown): string {
+    if (!date || !isString(date) || !isDate(date)) {
+        throw new Error('Incorrect or missing date' + date);
+    }
+
+    return date;
+}
+
+const parseWeather = (weather: unknown): Weather => {
+  if (!weather || !isString(weather) || !isWeather(weather)) {
+      throw new Error('Incorrect or missing weather: ' + weather);
+  }
+  return weather;
+};
+
+const parseVisibility = (visibility: unknown): Visibility => {
+  if (!visibility || !isString(visibility) || !isVisibility(visibility)) {
+      throw new Error('Incorrect or missing weather: ' + visibility);
+  }
+  return visibility;
+};
+
+const isString = (text: unknown): boolean => {
+  return typeof text === 'string' || text instanceof String;
+};
+
+const isDate = (date: string): boolean => {
+    return Boolean(Date.parse(date)); 
+}
+
+const isWeather = (str: string): str is Weather => {
+  return ['sunny', 'rainy', 'cloudy', 'stormy'].includes(str);
+};
+
+const isVisibility = (str: string): str is Visibility => {
+  return ['great' , 'good' , 'ok' , 'poor'].includes(str);
 };
