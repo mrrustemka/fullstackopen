658862f7b92d7890827c6e735e2798fbc87a761d
mrrustemka <mubarakshin1996@gmail.com>
2026-01-24 12:32:42 +0500

    part-9-c-parsing-request-body-in-middleware

--- part9/ilari/src/routes/diaries.ts
+++ part9/ilari/src/routes/diaries.ts
@@ -1,12 +1,13 @@
-﻿import express from 'express';
+﻿import express, { Request, Response, NextFunction } from 'express';
 import diaryService from '../services/diaryService';
 import { NewEntrySchema } from '../utils';
 
 import { z } from 'zod';
+import { NewDiaryEntry, DiaryEntry } from '../types';
 
 const router = express.Router();
 
-router.get('/', (_req, res) => {
+router.get('/', (_req, res: Response<DiaryEntry[]>) => {
   res.send(diaryService.getNonSensitiveEntries());
 });
 
@@ -20,22 +21,41 @@ router.get('/:id', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+const newDiaryParser = (req: Request, _res: Response, next: NextFunction) => {
   try {
-    const newDiaryEntry = NewEntrySchema.parse(req.body);
-    const addedEntry = diaryService.addDiary(newDiaryEntry);
-    res.json(addedEntry);
+    NewEntrySchema.parse(req.body);
+    console.log(req.body);
+    next();
   } catch (error: unknown) {
-    if (error instanceof z.ZodError) {
-      res.status(400).send({ error: error.issues });
-    } else {
-      res.status(400).send({ error: 'unknown error' });
-    }
+    next(error);
   }
-});
+};
 
-router.post('/', (_req, res) => {
-  res.send('Saving a diary!');
-});
+const errorMiddleware = (
+  error: unknown,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (error instanceof z.ZodError) {
+    res.status(400).send({ error: error.issues });
+  } else {
+    next(error);
+  }
+};
+
+router.post(
+  '/',
+  newDiaryParser,
+  (
+    req: Request<unknown, unknown, NewDiaryEntry>,
+    res: Response<DiaryEntry>
+  ) => {
+    const addedEntry = diaryService.addDiary(req.body);
+    res.json(addedEntry);
+  }
+);
+
+router.use(errorMiddleware);
 
 export default router;
